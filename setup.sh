eacda7c7aa66d4eb815828515647daf6b6aa9491
ni.yadav <nitin.yadav@geminisolution.in>
2021-03-25 06:11:39 +0530

    added auth0 info to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
 DATABASE_URL=postgres://omhpcszryqlxiq:4e70abbed25030b43c4940c9814b00b2317c8fc4e81361bbd594284f9ddf7bb8@ec2-54-205-183-19.compute-1.amazonaws.com:5432/dd288eui8smuok
+AUTH0_DOMAIN=nitin15067.us.auth0.com
+ALGORITHMS=RS256
+API_AUDIENCE=capstone
